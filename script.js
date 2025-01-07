32e7496f0ef9a0983d5a675c55a738407431a25f
foyzan <foyzanahmedapon01@gmail.com>
2025-01-07 12:55:44 +0600

    cursor project done

--- script.js
+++ script.js
@@ -2,16 +2,33 @@ const main = document.querySelector('#main');
 
 const cursor = document.querySelector('#cursor');
 
+const imgDiv = document.querySelector('#img');
 
 main.addEventListener('mousemove', (e) => {
     gsap.to(cursor, {
        x:`${e.x}px`,
        y:`${e.y}px`,
-       duration: 1,
-       ease: 'power4.out',
-         
+       duration: 1,    
     });
     
 });
 
+imgDiv.addEventListener('mouseenter', () => {
+    
+    cursor.innerHTML = "View more"
+    gsap.to(cursor, {
+        scale: 2,
+        duration: 0.5,
+        ease: 'power4.out',
+    });
+});
+
+imgDiv.addEventListener('mouseleave', () => {
+    cursor.innerHTML = "";
+    gsap.to(cursor, {
+        scale: 1,
+        duration: 0.5,
+        ease: 'power4.out',
+    });
+}); 
 
